ac60b645cbfa890e1236e38ffdc92200e9cef349
Jan Alexander Steffens (heftig) <heftig@archlinux.org>
2025-10-27 20:54:07 +0800

    ZEN: INTERACTIVE: Tune CFS for interactivity
    5.7:
    Take "sysctl_sched_nr_migrate" tune from early XanMod builds of 128. As
    of 5.7, XanMod uses 256 but that may affect applications that require
    timely response to IRQs.
    
    5.15:
    Per [a comment][1] on our ZEN INTERACTIVE commit, reducing the cost of
    migration causes the system less responsive under high load.  Most
    likely the combination of reduced migration cost + the higher number of
    tasks that can be migrated at once contributes to this.
    
    To better handle this situation, restore the mainline migration cost
    value and also reduce the max number of tasks that can be migrated in
    batch from 128 to 64.
    
    If this doesn't help, we'll restore the reduced migration cost and keep
    total number of tasks that can be migrated at once to 32.
    
    [1]: be5ba23#commitcomment-63159674
    
    6.6:
    Port the tuning to EEVDF, which removed a couple of settings.
    
    6.7:
    Instead of increasing the number of tasks that migrate at once, migrate
    the amount acceptable for PREEMPT_RT, but reduce the cost so migrations
    occur more often.
    
    This should make CFS/EEVDF behave more like out-of-tree schedulers that
    aggressively use idle cores to reduce latency, but without the jank
    caused by rebalancing too many tasks at once.
    
    EEVDF -> CFS
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -73,9 +73,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(sched_stat_runtime);
  *
  * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_latency			= 6000000ULL;
+unsigned int sysctl_sched_latency			= 4000000ULL;
 EXPORT_SYMBOL_GPL(sysctl_sched_latency);
-static unsigned int normalized_sysctl_sched_latency	= 6000000ULL;
+static unsigned int normalized_sysctl_sched_latency	= 4000000ULL;
 
 /*
  * The initial- and re-scaling of tunables is configurable
@@ -95,9 +95,9 @@ unsigned int sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
  *
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity			= 750000ULL;
+unsigned int sysctl_sched_min_granularity			= 400000ULL;
 EXPORT_SYMBOL_GPL(sysctl_sched_min_granularity);
-static unsigned int normalized_sysctl_sched_min_granularity	= 750000ULL;
+static unsigned int normalized_sysctl_sched_min_granularity	= 400000ULL;
 
 /*
  * Minimal preemption granularity for CPU-bound SCHED_IDLE tasks.
@@ -111,7 +111,7 @@ EXPORT_SYMBOL_GPL(sysctl_sched_idle_min_granularity);
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 8;
+static unsigned int sched_nr_latency = 10;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
@@ -128,9 +128,9 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  *
  * (default: 1 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_wakeup_granularity			= 1000000UL;
+unsigned int sysctl_sched_wakeup_granularity			= 500000UL;
 EXPORT_SYMBOL_GPL(sysctl_sched_wakeup_granularity);
-static unsigned int normalized_sysctl_sched_wakeup_granularity	= 1000000UL;
+static unsigned int normalized_sysctl_sched_wakeup_granularity	= 500000UL;
 
 const_debug unsigned int sysctl_sched_migration_cost	= 20000UL;
 
@@ -183,7 +183,7 @@ int __weak arch_asym_cpu_priority(int cpu)
  *
  * (default: 5 msec, units: microseconds)
  */
-static unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
+static unsigned int sysctl_sched_cfs_bandwidth_slice		= 3000UL;
 #endif
 
 #ifdef CONFIG_SYSCTL
